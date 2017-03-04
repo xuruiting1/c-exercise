aca21326c5ad0aa7eafb60f05ad57722aa29c8a6
徐瑞婷 <1263183452@qq.com>
2017-03-04 12:16:41 +0800

    法老的金字塔的提交

---
+++ 任务2.c
@@ -0,0 +1,22 @@
+/**
+说明：任务01_2法老的金字塔
+姓名：徐瑞婷
+学号：723
+班级：8班
+日期：2017/03/04
+*/
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main (void)
+//{
+//	printf("   *\n");
+//
+//	printf("  ***\n");
+//
+//	printf(" *****\n");
+//
+//	printf("********\n");
+//	system("pause");
+//	return 0;
+//}
