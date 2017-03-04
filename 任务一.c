173f2674bd01f3a67b57316762d220608abb81e4
徐瑞婷 <1263183452@qq.com>
2017-03-04 12:15:02 +0800

    向c语言问好的内容

---
+++ 任务一.c
@@ -0,0 +1,15 @@
+/**
+说明：任务01_1向C语言问好
+姓名：徐瑞婷
+学号：723
+班级：8班
+日期：2017/03/03
+*/
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main(void)
+//{
+//	printf("Nice to meet you,C!I am xuruiting.");
+//	system("pause");
+//	return 0;
+//}
